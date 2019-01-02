59c5745fb45031ecb6953db1af6619f49050d868
Jeff Jetton <jeff.jetton@gmail.com>
2019-01-02 10:17:50 -0600

    Clean up comments

--- bbones2.asm
+++ bbones2.asm
@@ -1,16 +1,24 @@
-        processor 6502
+; In dasm (and most other assemblers) comments look like this
+; They start with a semicolon and continue to the end of the line
 
-COLUBK  equ $09         ; This line is new
-PF1     equ $0E         ; As is this one
+; From here on out, we're indenting instructions by some consistent
+; number of spaces, to leave room for identifier text on the left
+
+       processor 6502
+
+; Create a new symbol (COLUBK) that "equates" to value $09
+COLUBK  equ $09
 
         org $F000
 
-Start   lda #$3C        ; We added a label to this line
-                        ; (And changed the hex value)
-        sta COLUBK      ; Slight change here
+Start   lda #$3C        ; Load a new color value into register A
+                        ; Note that we've marked the location of this
+                        ; instruction with the label "Start".
 
-        jmp Start       ; Making use of the label
+        sta COLUBK      ; Store A in whatever address COLUBK refers to
 
+        jmp Start       ; Jump to whatever address Start refers to
+        
         org $FFFC
-        .word Start     ; More label usage
-        .word Start     ; Here too!
+        .word Start     ; Begin at the "Start" location when reset
+        .word Start     ; Use that same address for interrupts
