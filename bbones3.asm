3c479c807482b56f19d93445d2354a32fb18f490
Jeff Jetton <jeff.jetton@gmail.com>
2019-01-02 10:20:41 -0600

    Remove unused equate

--- bbones3.asm
+++ bbones3.asm
@@ -1,10 +1,9 @@
         processor 6502
 
 COLUBK  equ $09
-PF1     equ $0E
 
         org $F000
-Start   sei           ; Prevents interrupts
+Start   sei           ; Prevent interrupts
         cld           ; Turn off decimal math
         ldx  #$FF     ; Take the highest address in RAM...
         txs           ; ...and put it in the stack pointer
@@ -12,7 +11,7 @@ Start   sei           ; Prevents interrupts
         lda  #$00     ; Begin initialization
 Init    sta  $00,x    ; Put A's zero into address $00 + X
         dex           ; Decrement X
-        bne  Init     ; Loop until X is zero
+        bne  Init     ; Loop as long as X isn't zero
         sta  $00      ; A final zero into address $00
 
 
