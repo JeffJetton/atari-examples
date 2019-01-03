c4bfef3f8f3577f0905c3af9e9fce35de2905d7a
Jeff Jetton <jeff.jetton@gmail.com>
2019-01-03 07:00:42 -0600

    Initial add

---
+++ 03 Refining the Bare-Bones Program.md
@@ -0,0 +1,19 @@
+# Refining the Bare-Bones Program
+
+### Code Files:
+* bbones2.asm
+* bbones3.asm
+
+Text goes here
+
+
+## Review
+
+* Labels
+* Initialization Routine
+   * Branches
+   * Loops
+
+Pretty simple, eh? 
+
+Next, we'll get the VCS to display some *very* simple output...
---
+++ rudout1.asm
@@ -0,0 +1,54 @@
+;---------------------------------------------------------------
+;
+; Rudimentary output, part 1
+; 
+; Display a static number in binary using playfield graphics
+;
+;---------------------------------------------------------------
+
+        processor 6502
+
+; Define a bunch of TIA addresses
+COLUBK  equ $09         ; Background color register
+PF1     equ $0E         ; Playfield register 1
+COLUPF  equ $08         ; Playfield color register
+
+		
+; Define some color constants (NTSC)
+MyBGCol equ $06         ; Medium gray
+MyPFCol equ $46         ; Reddish-pink
+
+
+; Initialize everything
+        org $F000
+Start   sei             ; Prevents interrupts
+        cld             ; Turn off decimal math
+        ldx  #$FF
+        txs             ; Set stack pointer
+
+        lda  #$00       ; Begin zeroing out TIA & RAM
+Init    sta  $00,x
+        dex
+        bne  Init
+        sta  $00
+
+
+; Set colors and playfield
+SetCols lda #MyBGCol
+        sta COLUBK
+        lda #MyPFCol
+        sta COLUPF
+
+        ; Load A with the number we want
+		; and show it in the playfield
+        lda #%10110111  ; <- Experiment by changing this value
+        sta PF1
+
+        jmp SetCols     ; Do it all over again...
+
+        org $FFFC
+        .word Start
+        .word Start
+
+
+
