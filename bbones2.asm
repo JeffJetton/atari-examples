c48355733c0dc285d386364d6d84ecdff93a81a2
Jeff Jetton <jeff.jetton@gmail.com>
2019-01-06 16:00:40 -0600

    Tweak numbering in spots

--- bbones2.asm
+++ bbones2.asm
@@ -1,10 +1,11 @@
 ; In dasm (and most other assemblers) comments look like this
 ; They start with a semicolon and continue to the end of the line
 
-       processor 6502
+        processor 6502
 
 ; Create a new symbol (COLUBK) that "equates" to value $09
 COLUBK  equ $09
+
 
         org $F000
 
@@ -15,7 +16,8 @@ Start   lda #$3C        ; Load a new color value into register A
         sta COLUBK      ; Store A in whatever address COLUBK refers to
 
         jmp Start       ; Jump to whatever address Start refers to
-        
+
+
         org $FFFC
         .word Start     ; Begin at the "Start" location when reset
         .word Start     ; Use that same address for interrupts
