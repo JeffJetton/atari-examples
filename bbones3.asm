404ff6c366edb935980e94203ea8eabba17ad1fb
Jeff Jetton <jeff.jetton@gmail.com>
2019-01-13 17:22:22 -0600

    Edits

--- bbones3.asm
+++ bbones3.asm
@@ -1,25 +1,26 @@
         processor 6502
 
 COLUBK  equ $09
+MyBGCol equ $2A
 
         org $F000
 
 
-Start   sei           ; Prevent interrupts
-        cld           ; Turn off decimal math
-        ldx  #$FF     ; Take the highest address in RAM...
-        txs           ; ...and put it in the stack pointer
+Start   sei         ; Prevent interrupts
+        cld         ; Clear decimal mode
+        ldx  #$FF   ; Take the highest address in RAM...
+        txs         ; ...and put it in the stack pointer
 
-        lda  #0       ; Begin initialization
-Init    sta  0,x      ; Put A's zero into address $00 + X
-        dex           ; Decrement X
-        bne  Init     ; Loop as long as X isn't zero
-        sta  $00      ; A final zero into address $00
+        lda  #0     ; Begin initialization
+Init    sta  0,x    ; Put A's zero into address $00 + X
+        dex         ; Decrement X
+        bne  Init   ; Loop as long as X isn't zero
+        sta  $00    ; A final zero into address $00
 
 
 ; Set up the graphics, such as they are...
 
-SetTIA  lda #$2A
+SetTIA  lda #MyBGCol
         sta COLUBK
 
         jmp SetTIA
