f783b2d29783d8dd2d083946a0c0d6f0a655b151
Jeff Jetton <jeff.jetton@gmail.com>
2019-01-13 21:54:23 -0600

    Initial commit

---
+++ bbones4.asm
@@ -0,0 +1,32 @@
+;--------------------------------------------------------------
+;
+; Bare-Bones Program #4
+;
+; Using includes to save ourselves a lot of bother
+;
+;--------------------------------------------------------------
+
+        processor 6502
+
+        include "vcs.h"     ; Standard definitions for VCS code
+        include "macro.h"   ; Useful common macros
+
+MyBGCol equ $88
+
+        org $F000
+
+Start   CLEAN_START     ; Macro for efficient initialization
+                        ; Actual code comes from macro.h
+
+        ; Main loop
+SetTIA  lda #MyBGCol
+        sta COLUBK      ; Note that we didn't define COLUBK
+                        ; in this file. It's defined in vch.h
+
+        jmp SetTIA
+
+
+        org $FFFC
+        .word Start
+        .word Start
+
